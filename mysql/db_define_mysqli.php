32871bab42533cdb44fa7735390b7f5054459139
centrofriuli <informatici@centrofriuli.eu>
2022-10-24 17:22:45 +0200

    UPD: aggiornamenti fpdf

--- mysql/db_define_mysqli.php
+++ mysql/db_define_mysqli.php
@@ -4,6 +4,7 @@
 	define('MYSQL_db_user','root');
 	define('MYSQL_db_psw','Inf0rm4t1c1');
 	define('MYSQL_db_name','produzione_cf');
+//    test
 	
 	//Collegamento al database
 	$db = mysqli_connect(MYSQL_host, MYSQL_db_user, MYSQL_db_psw) or die ('Problema di collegamento al database');
